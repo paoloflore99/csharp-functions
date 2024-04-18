de89ffa80b59294f5235e2faea5b532725a740e9
paolo flore <paoloflore937@gmail.com>
2024-04-18 15:23:24 +0200

    sneck 1

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            void virgola()
+            {
+                Console.Write(" , ");
+            }
+
+
+
+            void StampaArray(int[] array)
+            {
+                
+                Console.Write("[ ");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                    {
+                        virgola();
+                    }
+                    
+                }
+                
+                Console.Write(" ] ");
+            }
+
+            int[] numeri = { 1,2,3,4,5,6,7,8,9 };
+            StampaArray(numeri);
+
+
+
+            
         }
     }
 }
